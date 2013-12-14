cc32d1a894f467ea63c85b6ad8bec2d4fa0690e5
jleu1656 <jleu1656@gmail.com>
2013-12-14 20:30:16 +1100

    Main game activity changes

--- MysteryTetris/src/com/hjcubed/mysterytetris/MainGameActivity.java
+++ MysteryTetris/src/com/hjcubed/mysterytetris/MainGameActivity.java
@@ -1,11 +1,21 @@
 package com.hjcubed.mysterytetris;
 
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 
-public class MainGameActivity extends Activity {
-
+public class MainGameActivity extends SimpleBaseGameActivity {
+	Camera camera; 
+	Scene scene; 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +29,32 @@ public class MainGameActivity extends Activity {
         getMenuInflater().inflate(R.menu.main_game, menu);
         return true;
     }
+
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		DisplayMetrics metrics = getResources().getDisplayMetrics();
+		int width  = metrics.widthPixels;
+		int height = metrics.heightPixels;
+		camera = new Camera(0, 0, width, height);
+		 
+		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED,
+		new RatioResolutionPolicy(width, height), camera);
+	}
+
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	protected Scene onCreateScene() {
+		scene = new Scene(); 
+		scene.setBackground(new Background(0,0,0));
+		return scene;
+	}
     
 }
